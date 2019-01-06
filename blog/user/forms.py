ad53b01b48e7c37343fa422b78f960063c2f91b6
ludan <1625152465@qq.com>
2019-01-06 13:55:21 +0800

    强密码表单

--- blog/user/forms.py
+++ blog/user/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import MyUser
 ####
 from captcha.fields import CaptchaField
 
@@ -7,9 +9,19 @@ class CaptchaLoginForm(forms.Form):
     password = forms.CharField(label='登录密码', widget=forms.PasswordInput,error_messages={"invalid":u"密码填写错误"})
     captcha = CaptchaField(label='验证码',error_messages={"invalid":u"验证码错误"})
 
-class CaptchaRegistForm(forms.Form):
-    username = forms.CharField(label='用户名',error_messages={"invalid":u"用户名已经注册过"})
-    email = forms.EmailField(label='邮箱地址',error_messages={"invalid":u"邮箱已经注册过"})
-    password1 = forms.CharField(label='密码', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='确定密码', widget=forms.PasswordInput)
+class MyUserCreationForm(UserCreationForm):
+    # 重写初始化函数，设置自定义字段password1和password2的样式和属性
     captcha = CaptchaField(label='验证码',error_messages={"invalid":u"验证码错误"})
+    def __init__(self, *args, **kwargs):
+        super(MyUserCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password1'].widget = forms.PasswordInput(attrs={'placeholder':'密码,8-16位数字/字母/特殊符号(空格除外)'})
+        self.fields['password2'].widget = forms.PasswordInput(attrs={'placeholder':'重复密码'})
+    class Meta(UserCreationForm.Meta):
+        model = MyUser
+        # 在注册界面添加模型字段：手机号码和密码
+        fields = UserCreationForm.Meta.fields +('email',)
+        # 设置模型字段的样式和属性
+        widgets = {
+            'email': forms.widgets.TextInput(attrs={'class': 'txt tabInput', 'placeholder':'邮箱'}),
+            'username': forms.widgets.TextInput(attrs={'class': 'txt tabInput', 'placeholder':'用户名'}),
+        }
